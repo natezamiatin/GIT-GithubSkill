b840e24e08adebe12662eca12bbf5d0041a719e0
Nikita Zamiatin <4406015@gmail.com>
2019-11-08 14:17:39 -0500

    some commands about Branch Merge and Fork, Pull

--- src/App.js
+++ src/App.js
@@ -9,8 +9,18 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          Lets improve our GIT/Github skill 
+
+
         </p>
             </header>
+
+            <h1>
+              Branch its when we create a new line on the tree 
+              Merge when we add Branch to the master line 
+              Fork its a clone whole a repo and added to your account 
+              Pull request mean send your changes to the main repo 
+
+            </h1>
     </div>
   );
 }
