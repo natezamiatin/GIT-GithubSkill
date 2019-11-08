acae81d591871183d9ce8bf498b1b3f89e723423
Nikita Zamiatin <4406015@gmail.com>
2019-11-08 14:30:57 -0500

    terminal commands

--- src/App.js
+++ src/App.js
@@ -8,9 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Here is will be my all commands about Git and GitHub 
+         {/* Here is will be my all commands about Git and GitHub 
+         ## Working with Git
 
+            ### Quick Start
+            git clone <url> 					# Clone directory
+            git checkout -b <new-branch> 		# Create new local branch
+            git push -u origin <new-branch> 	# Sync local branch with remote
+            git checkout <branch> 				# Checkout branch
+            git push origin <branch> 			# Push branch to remote
 
+            git branch -d <branchname>   	# deletes local branch
+            git push origin :<branchname>	# deletes remote branch
+
+            git subtree push --prefix docs origin gh-pages  # push docs as subtree to gh-pages */}
         </p>
             </header>
 
