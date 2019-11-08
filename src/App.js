c8054e6d2556a12133a7b42cd5ca779993975f48
Nikita Zamiatin <4406015@gmail.com>
2019-11-08 14:23:50 -0500

    start working on branch Unicorn

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Lets improve our GIT/Github skill 
+         Here is will be my all commands about Git and GitHub 
 
 
         </p>
