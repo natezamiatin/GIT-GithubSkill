45d30bef857094d500cefc0e2d4928683d781484
Nate Zamiatin <4406015@gmail.com>
2019-11-08 13:30:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Lets improve our GIT/Github skill 
+        This is a Unicorn branch 
         </p>
             </header>
     </div>
